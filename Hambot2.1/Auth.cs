11f6e63116037b0fb58c9cd1cf953d1b64354972
Dephinain <dephinain@gmail.com>
2019-09-29 13:17:17 -0500

    was dumb and didn't remove keys haHAA

--- Hambot2.1/Auth.cs
+++ Hambot2.1/Auth.cs
@@ -6,8 +6,8 @@ namespace Hambot2._1
 {
     public class Auth
     {
-        private const string discord_token = "MzU4NzUxMjMxMTA5MjM0Njkx.Du-T4A.YPl8pRh58huwH7yZv1IpRwK3lNI";
-        private const string youtube_key = "AIzaSyBXdQH37Et__lj6dS6pfWTV9Io3lG7xpFY";
+        private const string discord_token = "<insert token here";
+        private const string youtube_key = "<insert token here>";
 
         public string GetDiscordToken()
         {
