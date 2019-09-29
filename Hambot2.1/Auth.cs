ec8d45a8caf2e5a7076f910c2d244d72754dd0c3
Dephinain <dephinain@gmail.com>
2019-09-29 15:31:14 -0500

    test

--- Hambot2.1/Auth.cs
+++ Hambot2.1/Auth.cs
@@ -6,7 +6,7 @@ namespace Hambot2._1
 {
     public class Auth
     {
-        private const string discord_token = "test";
+        private const string discord_token = "tegst";
         private const string youtube_key = "nope";
 
         public string GetDiscordToken()
